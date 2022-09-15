4218310b56be774cc25acc128d331a70564380de
Chris <46506838+MotorcycleCapitalist@users.noreply.github.com>
2022-09-14 22:34:28 -0500

    url_shortening (m3w6)

---
+++ express_server.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const app = express();
+const PORT = 3000; // default port 8080
+app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs")
+
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase};
+  res.render("urls_index", templateVars);
+});
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+}); 
+
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase.id };
+  res.render("urls_show", templateVars);
+}); 
+
+app.get("/urls.json", (req, res) => {
+    res.json(urlDatabase);
+  });
+                      
+
+  app.get("/hello", (req, res) => {
+    res.send("<html><body>Hello <b>World</b></body></html>\n");
+  });                         
+  app.get("/set", (req, res) => {
+    const a = 1;
+    res.send(`a = ${a}`);
+   });
+   
+   app.get("/fetch", (req, res) => {
+    res.send(`a = ${a}`);
+   });
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
+function generateRandomString(){}
