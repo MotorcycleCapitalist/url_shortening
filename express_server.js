d01ef7826110e8fa179845e6141510e2f92f69e7
Chris <46506838+MotorcycleCapitalist@users.noreply.github.com>
2022-09-15 21:49:34 -0500

    express_server.js
    URL shortening (p2)

--- express_server.js
+++ express_server.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const app = express();
-const PORT = 3000; // default port 8080
+const PORT = 3001; // default port 8080
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs")
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "b2xVn2": "https://www.lighthouselabs.ca",
+  "9sm5xK": "https://www.google.com"
 };
-
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -16,12 +19,16 @@ app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase};
   res.render("urls_index", templateVars);
 });
+app.get("/u/:id", (req, res) => {
+ 
+  const longURL = urlDatabase[req.params.id]
+  res.render(longURL);
+}); 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 }); 
-
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase.id };
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 }); 
 
@@ -45,9 +52,6 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
-});
+
 
 function generateRandomString(){}
